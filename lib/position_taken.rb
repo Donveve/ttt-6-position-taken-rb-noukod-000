59c8aaaf35bac3296288e6ebae3ec40eb409acbe
Veniel Dorestave <veniel@noukod.com>
2018-08-31 14:58:28 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,4 +1,3 @@
-# code your #position_taken? method here!
-def position_taken(board,index)
-  ! (board[index] ==  " " || board[index] == "" || board[index] == nil)
+def position_taken?(board, index)
+    ! (board[index] ==  " " || board[index] == "" || board[index] == nil)
 end
